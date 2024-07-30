3b264dcf4c9011d77cbe5878ac3915512aad2d07
bitcriptoinvest <145938809+bitcriptoinvest@users.noreply.github.com>
2024-07-30 00:08:56 -0300

    Update addresses.ts

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -4,31 +4,31 @@ import { SupportedChainId } from 'constants/chains'
  * Map of supported chains to USDC contract addresses
  */
 export const CHAIN_IDS_TO_USDC_ADDRESSES = {
-  [SupportedChainId.ETH_SEPOLIA]: '0x1c7d4b196cb0c7b01d743fbc6116a902379c7238',
+  [SupportedChainId.ETH_SEPOLIA]: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
 
-  [SupportedChainId.AVAX_FUJI]: '0x5425890298aed601595a70AB815c96711a31Bc65',
+  [SupportedChainId.AVAX_FUJI]: '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E',
 
-  [SupportedChainId.ARB_SEPOLIA]: '0x75faf114eafb1bdbe2f0316df893fd58ce46aa4d',
+  [SupportedChainId.ARB_SEPOLIA]: '(USDCFORTRESS)',
 }
 
 /**
  * Map of supported chains to Token Messenger contract addresses
  */
 export const CHAIN_IDS_TO_TOKEN_MESSENGER_ADDRESSES = {
-  [SupportedChainId.ETH_SEPOLIA]: '0x9f3b8679c73c2fef8b59b4f3444d4e156fb70aa5',
+  [SupportedChainId.ETH_SEPOLIA]: '0xBd3fa81B58Ba92a82136038B25aDec7066af3155',
 
-  [SupportedChainId.AVAX_FUJI]: '0xeb08f243e5d3fcff26a9e38ae5520a669f4019d0',
+  [SupportedChainId.AVAX_FUJI]: '0x6B25532e1060CE10cc3B0A99e5683b91BFDe6982',
 
-  [SupportedChainId.ARB_SEPOLIA]: '0x9f3b8679c73c2fef8b59b4f3444d4e156fb70aa5',
+  [SupportedChainId.ARB_SEPOLIA]: '(USDCMESSENGERFORTRESS)',
 }
 
 /**
  * Map of supported chains to Message Transmitter contract addresses
  */
 export const CHAIN_IDS_TO_MESSAGE_TRANSMITTER_ADDRESSES = {
-  [SupportedChainId.ETH_SEPOLIA]: '0x7865fafc2db2093669d92c0f33aeef291086befd',
+  [SupportedChainId.ETH_SEPOLIA]: '0x0a992d191deec32afe36203ad87d7d289a738f81',
 
-  [SupportedChainId.AVAX_FUJI]: '0xa9fb1b3009dcb79e2fe346c16a604b8fa8ae0a79',
+  [SupportedChainId.AVAX_FUJI]: '0x8186359aF5F57FbB40c6b14A588d2A59C0C29880',
 
-  [SupportedChainId.ARB_SEPOLIA]: '0xacf1ceef35caac005e15888ddb8a3515c41b4872',
+  [SupportedChainId.ARB_SEPOLIA]: '(USDCTRANSMITTERFORTRESS)',
 }
