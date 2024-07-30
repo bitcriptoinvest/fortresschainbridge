37b1db9acd7c9b7fe64275c12b5ee15d0112df43
bitcriptoinvest <145938809+bitcriptoinvest@users.noreply.github.com>
2024-07-30 02:05:45 -0300

    Update Send.tsx

--- src/pages/Send/Send.tsx
+++ src/pages/Send/Send.tsx
@@ -70,7 +70,7 @@ function Send() {
   return (
     <>
       <div className="item-center mx-auto flex max-w-4xl flex-col justify-center">
-        <h1>Transfer USDC across chains</h1>
+        <h1>Transfer USDC to FortressChain</h1>
         <p className="mt-8 text-center text-xl">
           Circle&apos;s Cross-Chain Transfer Protocol enables USDC to be sent
           across blockchains without the need to be converted into a asset. All
